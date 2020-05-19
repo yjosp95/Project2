e64f726e63abb1e39521486eb7c62df71906aab0
Yun Jae Oh <52719017+yjosp95@users.noreply.github.com>
2020-05-19 22:27:17 +0900

    6번 함수 수정했습니다

--- main.c
+++ main.c
@@ -57,9 +57,12 @@ void sizecmp(){
 }
 
 //두 개의 파일 블락 수를 비교하는 함수 작성
-void blockcmp(){
-    
+void blockcmp(void)
+{
+	printf("block compare\n");
+	(int)stat1.st_blocks> (int)stat2.st_blocks ? printf("text1 is bigger\n") : printf("text2 is bigger\n");
 }
+
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
 void datecmp(){
